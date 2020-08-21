f937e9b8189587999276a18a2e9b24164aaa5586
SS Jones <SSJones.code@gmail.com>
2020-08-20 17:28:10 -0700

    I added the .js file and inserted a template to follow to get element by id and gathetr the information that the user inputs.

---
+++ script.js
@@ -0,0 +1,12 @@
+var targetDiv = document.getElementById("empty-div");
+
+      targetDiv.textContent = "Hello friends!";
+
+      // In this example, we instead create a new div called "newDiv".
+      var newDiv = document.createElement("div");
+
+      // We then give this newDiv the text "A pleasure to meet you!".
+      newDiv.textContent = "A pleasure to meet you!";
+
+      // Now we use the ".appendChild" method to combine the two divs together on the page.
+      targetDiv.appendChild(newDiv);
